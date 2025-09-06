0684395e045b8139848bb416759315a1c09d3bfd
Ricky Kim <rickykim2013.dev@gmail.com>
2025-09-05 19:19:12 -0500

    toast center

--- client/src/components/ui/toast.tsx
+++ client/src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-0 right-0 z-[100] flex max-h-screen w-full flex-col p-4 md:max-w-[420px]",
+      "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[100] flex max-h-screen w-full flex-col p-4 max-w-[420px]",
       className
     )}
     {...props}
